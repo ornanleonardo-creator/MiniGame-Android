7205defe291e03fc71ddf4d09f03f9ea86373674
ornanleonardo-creator <ornanleonardo@gmail.com>
2026-01-21 12:49:38 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/monapp/MainActivity.java
+++ app/src/main/java/com/example/monapp/MainActivity.java
@@ -1,26 +1,12 @@
 package com.example.monapp;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    int score = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView scoreText = findViewById(R.id.scoreText);
-        Button clickButton = findViewById(R.id.clickButton);
-
-        clickButton.setOnClickListener(v -> {
-            score++;
-            scoreText.setText("Score : " + score);
-        });
     }
 }
